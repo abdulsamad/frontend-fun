b93132ed495b50d8a6eb803eac0c9f5eb5907e79
Abdul Samad <Samk8778@yahoo.com>
2021-06-11 16:33:36 +0530

    Changed split color

--- src/components/output/Output.ts
+++ src/components/output/Output.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 const Output = styled.section`
   background-color: #ffffff;
-  border-left: 2px solid #2f2f2f;
 `;
 
 export default Output;
--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -6,6 +6,17 @@ const GlobalStyle = createGlobalStyle`
 		width: 100vw;
 		overflow: hidden;
   }
+
+  .gutter {
+    background-color: #3B3B3B;
+    background-repeat: no-repeat;
+    background-position: 50%;
+  }
+
+  .gutter.gutter-horizontal {
+    background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAeCAYAAADkftS9AAAAIklEQVQoU2M4c+bMfxAGAgYYmwGrIIiDjrELjpo5aiZeMwF+yNnOs5KSvgAAAABJRU5ErkJggg==');
+    cursor: col-resize;
+  }
 `;
 
 export default GlobalStyle;
