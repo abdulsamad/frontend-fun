17adec01bf10442835f671b6e45d29dbd5837df0
Abdul Samad <Samk8778@yahoo.com>
2021-06-12 19:10:39 +0530

    Fixed parsing error in serverless function

--- api/saveFilesData.js
+++ api/saveFilesData.js
@@ -8,19 +8,21 @@ mongoose.connect(process.env.DATABASE_URI, {
 });
 
 module.exports = (req, res) => {
+  const body = JSON.parse(req.body);
+
   if (req.method !== 'POST') return res.json({ err: 'Only POST requests allowed.' });
 
   const { _id } = req.query;
 
   // _id should be parsed and validated for security reasons
   if (_id) {
-    filesDataModel.findOneAndUpdate({ _id }, req.body).exec((err, data) => {
+    filesDataModel.findOneAndUpdate({ _id }, JSON.parse(body)).exec((err, data) => {
       if (err) return res.json({ err });
 
       return res.json(data);
     });
   } else {
-    const filesData = new filesDataModel(req.body);
+    const filesData = new filesDataModel(body);
 
     filesData.save((err, data) => {
       if (err) return res.json({ err });
