81945da94f1eb4ae5a11d856c4f7316b59e6b004
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-03-12 01:51:45 +0530

    :bug: Fixed mongoDB not working

--- functions/db.ts
+++ functions/db.ts
@@ -1,30 +1,27 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 mongoose.Promise = global.Promise;
 
 let isConnected: any;
 
 export const connectToDatabase = async () => {
-  try {
-    if (isConnected) {
-      console.log("=> Using existing database connection");
-      return Promise.resolve();
-    }
+	try {
+		if (isConnected) {
+			console.log('=> Using existing database connection');
+			return Promise.resolve();
+		}
 
-    console.log("=> Using new database connection");
+		console.log('=> Using new database connection');
 
-    const conn = await mongoose.connect(process.env.DATABASE_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+		const conn = await mongoose.connect(process.env.DATABASE_URI);
 
-    isConnected = conn.connections[0].readyState;
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        err: "Unable to establish a database connection!",
-      }),
-    };
-  }
+		isConnected = conn.connections[0].readyState;
+	} catch (err) {
+		return {
+			statusCode: 500,
+			body: JSON.stringify({
+				err: 'Unable to establish a database connection!',
+			}),
+		};
+	}
 };
--- functions/saveFilesData.ts
+++ functions/saveFilesData.ts
@@ -1,59 +1,59 @@
-import { Handler } from "@netlify/functions";
+import { Handler } from '@netlify/functions';
 
-import { connectToDatabase } from "./db";
-import filesDataModel from "../model/FilesData";
+import { connectToDatabase } from './db';
+import filesDataModel from '../model/FilesData';
 
 const handler: Handler = async (event) => {
-  try {
-    const id = (event.queryStringParameters as any).id;
-    const body = JSON.parse(event.body as any);
+	try {
+		const id = (event.queryStringParameters as any).id;
+		const body = JSON.parse(event.body as any);
 
-    if (event.httpMethod !== "POST")
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ err: "Only POST requests allowed." }),
-      };
+		if (event.httpMethod !== 'POST')
+			return {
+				statusCode: 400,
+				body: JSON.stringify({ err: 'Only POST requests allowed.' }),
+			};
 
-    if (!body)
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ err: "No data found!" }),
-      };
+		if (!body)
+			return {
+				statusCode: 400,
+				body: JSON.stringify({ err: 'No data found!' }),
+			};
 
-    // Connect to DB
-    await connectToDatabase();
+		// Connect to DB
+		await connectToDatabase();
 
-    // id should validated for security reasons
-    if (id) {
-      // Update Saved Data
-      await filesDataModel.updateOne({ _id: id }, body).exec();
+		// id should validated for security reasons
+		if (id) {
+			// Update Saved Data
+			await filesDataModel.updateOne({ _id: id }, body).exec();
 
-      return {
-        statusCode: 201,
-        body: JSON.stringify({
-          id,
-          msg: "Successfully updated your data",
-        }),
-      };
-    }
+			return {
+				statusCode: 201,
+				body: JSON.stringify({
+					id,
+					msg: 'Successfully updated your data',
+				}),
+			};
+		}
 
-    // Create and Save New Data
-    const filesData = new filesDataModel(body);
-    const savedData = await filesData.save();
+		// Create and Save New Data
+		const filesData = new filesDataModel(body);
+		const savedData = await filesData.save();
 
-    return {
-      statusCode: 201,
-      body: JSON.stringify({
-        id: savedData._id,
-        msg: "Successfully saved your data",
-      }),
-    };
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ err: "Internal server error" }),
-    };
-  }
+		return {
+			statusCode: 201,
+			body: JSON.stringify({
+				id: savedData._id,
+				msg: 'Successfully saved your data',
+			}),
+		};
+	} catch (err) {
+		return {
+			statusCode: 500,
+			body: JSON.stringify({ err: 'Internal server error' }),
+		};
+	}
 };
 
 export { handler };
