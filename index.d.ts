f8daed2ad4d27b0f4eb84a31d40e0663d3a69f56
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-03-12 01:51:09 +0530

    :label: Updated env.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,5 @@
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      DATABASE_URI: string;
-    }
-  }
+declare namespace NodeJS {
+	interface ProcessEnv {
+		DATABASE_URI: string;
+	}
 }
-
-// convert it into a module by adding an empty export statement.
-export {};
