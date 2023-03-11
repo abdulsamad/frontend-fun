96d343107435837f6d33f199ec23e035d1485335
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-03-12 01:59:23 +0530

    :art: Improved formatting

--- functions/getFilesData.ts
+++ functions/getFilesData.ts
@@ -1,34 +1,35 @@
-import { Handler } from "@netlify/functions";
+import { Handler } from '@netlify/functions';
 
-import { connectToDatabase } from "./db";
-import filesDataModel from "../model/FilesData";
+import { connectToDatabase } from './db';
+import filesDataModel from '../model/FilesData';
 
 const handler: Handler = async (event) => {
-  try {
-    const id = (event.queryStringParameters as any).id;
+	try {
+		const id = (event.queryStringParameters as any).id;
 
-    if (event.httpMethod !== "GET")
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ err: "Only GET requests allowed." }),
-      };
+		if (event.httpMethod !== 'GET') {
+			return {
+				statusCode: 400,
+				body: JSON.stringify({ err: 'Only GET requests allowed.' }),
+			};
+		}
 
-    // Connect to DB
-    await connectToDatabase();
+		// Connect to DB
+		await connectToDatabase();
 
-    // id should be and validated for security reasons
-    const savedData = await filesDataModel.findOne({ _id: id }).exec();
+		// id should be and validated for security reasons
+		const savedData = await filesDataModel.findOne({ _id: id }).exec();
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(savedData),
-    };
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ err: "Internal server error" }),
-    };
-  }
+		return {
+			statusCode: 200,
+			body: JSON.stringify(savedData),
+		};
+	} catch (err) {
+		return {
+			statusCode: 500,
+			body: JSON.stringify({ err: 'Internal server error' }),
+		};
+	}
 };
 
 export { handler };
