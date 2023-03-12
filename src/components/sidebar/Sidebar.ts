188208958ebd7b8ad9e5c2d0bbbee991cbfb750b
AbdulSamad Ansari <hello@abdulsamad.dev>
2023-03-12 13:21:03 +0530

    :lipstick: Added sidebar minwidth

--- src/components/sidebar/Sidebar.ts
+++ src/components/sidebar/Sidebar.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const Sidebar = styled.section`
 	height: 100vh;
 	width: 100%;
+	min-width: 250px;
 	overflow: auto;
 	color: #f5f5f5;
 	background-color: #131313;
