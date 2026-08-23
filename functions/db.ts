3d67b4187f2eda0d6ea1b424a42e1a0c534a670b
Abdul Samad Ansari <hello@abdulsamad.dev>
2026-08-23 17:42:33 +0530

    Refactor database connection code for consistency in string formatting

--- functions/db.ts
+++ functions/db.ts
@@ -1,23 +1,23 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 mongoose.Promise = global.Promise;
 
 let isConnected = false;
 
 export const connectToDatabase = async () => {
-	try {
-		if (isConnected) {
-			console.log('=> Using existing database connection');
-			return Promise.resolve();
-		}
+  try {
+    if (isConnected) {
+      console.log("=> Using existing database connection");
+      return Promise.resolve();
+    }
 
-		console.log('=> Using new database connection');
+    console.log("=> Using new database connection");
 
-		const conn = await mongoose.connect(process.env.DATABASE_URI);
+    const conn = await mongoose.connect(process.env.DATABASE_URI!);
 
-		isConnected = conn.connections[0].readyState === 1;
-	} catch (err) {
-		isConnected = false;
-		throw new Error('Unable to establish a database connection');
-	}
+    isConnected = conn.connections[0].readyState === 1;
+  } catch (err) {
+    isConnected = false;
+    throw new Error("Unable to establish a database connection");
+  }
 };
